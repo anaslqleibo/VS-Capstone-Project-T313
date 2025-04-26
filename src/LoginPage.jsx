caba11271fdefd087c48842f8a4fadb355a3b7d7
Evanesce <anthoniusevan@gmail.com>
2025-04-26 19:05:39 +1000

    Fixed login-container not centered

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,16 +1,18 @@
 function LoginPage() {
     return (
-        <div className="login-container">
-            <h2>2 Bent Rods - Login</h2>
-            <form>
-                <label for="email">Email</label>
-                <input type="email" id="email" placeholder="Enter your email" required />
+        <div className="login-body">
+            <div className="login-container">
+                <h2>2 Bent Rods - Login</h2>
+                <form>
+                    <label for="email">Email</label>
+                    <input type="email" id="email" placeholder="Enter your email" required />
 
-                <label for="password">Password</label>
-                <input type="password" id="password" placeholder="Enter your password" required />
+                    <label for="password">Password</label>
+                    <input type="password" id="password" placeholder="Enter your password" required />
 
-                <button type="submit">Login</button>
-            </form>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
         </div>
     );
   }
